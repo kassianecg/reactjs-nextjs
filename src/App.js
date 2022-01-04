c20dd5acfe125773cc08883dffc17471549c3585
Kassiane CG <kassianecristinagoncalves@gmail.com>
2022-01-04 11:12:29 -0300

    add new features and config typescript file

--- src/App.js
+++ src/App.js
@@ -1,63 +1,31 @@
 import { Component } from 'react'
+
 import './App.css'
+
+import { Posts } from './components/Posts'
+import { loadPosts } from './utils/load-posts'
 
 class App extends Component {
 	state = {
-		counter: 0,
-		posts: [
-			{
-				id: 1,
-				title: 'O título 1',
-				body: 'O corpo 1'
-			},
-			{
-				id: 2,
-				title: 'O título 2',
-				body: 'O corpo 2'
-			},
-			{
-				id: 3,
-				title: 'O título 3',
-				body: 'O corpo 3'
-			}
-		]
-	}
-	timeoutUpdate = null
-
-	componentDidMount() {
-		this.handleTimeout()
+		posts: []
 	}
 
-	componentDidUpdate() {
-		this.handleTimeout()
+	async componentDidMount() {
+		await this.loadPosts()
 	}
 
-	componentWillUnmount() {
-		clearTimeout(this.timeoutUpdate)
-	}
-
-	handleTimeout = () => {
-		const { posts, counter } = this.state
-		posts[0].title = 'O título mudou'
-
-		this.timeoutUpdate = setTimeout(() => {
-			this.setState({ posts, counter: counter + 1 })
-		}, 1000)
+	loadPosts = async () => {
+		const postsAndPhotos = await loadPosts()
+		this.setState({ posts: postsAndPhotos })
 	}
 
 	render() {
-		const { posts, counter } = this.state
+		const { posts } = this.state
 
 		return (
-			<div className="App">
-				<h1>{counter}</h1>
-				{posts.map(post => (
-					<div key={post.id}>
-						<h1>{post.title}</h1>
-						<p>{post.body}</p>
-					</div>
-				))}
-			</div>
+			<section className="container">
+				<Posts posts={posts} />
+			</section>
 		)
 	}
 }
